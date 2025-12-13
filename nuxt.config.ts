33cbea7b3bc64c3aeec9eeaef02b7ab2669a4f66
Qian <git@qian.io>
2025-12-14 00:19:13 +0800

    feat: add electron, electron builder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  modules: ['nuxt-electron'],
   css: [
     '@/css/main.css'
   ],
   plugins: [
       '@/plugins/toast',
-  ]
+  ],
+  electron: {
+    build: [
+      {
+        entry: 'electron/main.ts'
+      },
+    ],
+    disableDefaultOptions: true,
+  },
+  ssr: false,
 })
