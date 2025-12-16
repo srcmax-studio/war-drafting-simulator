cb9a416673ab24c0286b6fd16507bde83cd9274d
Qian <git@qian.io>
2025-12-16 18:40:13 +0800

    chore: stop tracking dist-electron

--- dist-electron/main.js
+++
@@ -1,7 +0,0 @@
-import { app as e, BrowserWindow as n } from "electron";
-e.whenReady().then(() => {
-  const o = new n();
-  o.loadURL(process.env.VITE_DEV_SERVER_URL), o.once("ready-to-show", () => {
-    o.maximize(), o.show();
-  });
-});
